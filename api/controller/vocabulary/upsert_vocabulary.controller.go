38e2a4c9197f6abffdba110b9fd14c057ee2bc80
KuroNgo <hoaiphong01012002@gmail.com>
2024-03-12 20:19:38 +0700

    feat: vocabulary delete and upsert

---
+++ api/controller/vocabulary/upsert_vocabulary.controller.go
@@ -0,0 +1,43 @@
+package vocabulary_controller
+
+import (
+	vocabulary_domain "clean-architecture/domain/vocabulary"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func (v *VocabularyController) UpsertOneVocabulary(ctx *gin.Context) {
+	vocabularyID := ctx.Query("_id")
+
+	var vocabularyInput vocabulary_domain.Input
+	if err := ctx.ShouldBindJSON(&vocabularyInput); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status":  "error",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	upsertVocabulary := vocabulary_domain.Vocabulary{
+		Word:          vocabularyInput.Word,
+		PartOfSpeech:  vocabularyInput.PartOfSpeech,
+		Pronunciation: vocabularyInput.Pronunciation,
+		Example:       vocabularyInput.Example,
+		FieldOfIT:     vocabularyInput.FieldOfIT,
+		LessonID:      vocabularyInput.LessonID,
+	}
+
+	vocabularyRes, err := v.VocabularyUseCase.UpsertOne(ctx, vocabularyID, &upsertVocabulary)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"status":  "error",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{
+		"status": "success",
+		"data":   vocabularyRes,
+	})
+}
