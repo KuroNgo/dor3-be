eea05a5bc1043abe4078121b3dbe6b149f0f87ed
KuroNgo <hoaiphong01012002@gmail.com>
2024-06-18 16:11:56 +0700

    add casbin

--- api/controller/casbin/create_role.controller.go
+++ api/controller/casbin/create_role.controller.go
@@ -31,3 +31,87 @@ func AddRole(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusCreated, "success added role: "+data.Role)
 }
+
+func AddRoleForUser(ctx *gin.Context) {
+	var data UserRole
+
+	if err := ctx.ShouldBindJSON(&data); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status":  "fail",
+			"message": "can not get data",
+		})
+	}
+
+	for _, id := range data.UserID {
+		_, err := casbin.Rbac.AddGroupingPolicy(id, data.Role)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"status":  "fail",
+				"message": "can not add role for this user",
+			})
+		}
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"status": "success",
+	})
+}
+
+func AddRoleForAPI(ctx *gin.Context) {
+	var data APIData
+
+	if err := ctx.ShouldBindJSON(&data); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status":  "success",
+			"message": "can not get data",
+		})
+	}
+
+	for _, role := range data.Role {
+		for _, method := range data.Method {
+			_, err := casbin.Rbac.AddPolicy(role, data.API, method)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{
+					"status":  "fail",
+					"message": "can not add role for this user",
+				})
+			}
+		}
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"status": "success",
+	})
+}
+
+func AddAPIForRole(ctx *gin.Context) {
+	var data RoleData
+
+	if err := ctx.ShouldBindJSON(&data); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"status":  "success",
+			"message": "can not get data",
+		})
+	}
+
+	// nếu chưa có method nào thì thêm method mặc định GET
+	if data.Method == nil {
+		data.Method = append(data.Method, "GET")
+	}
+
+	for _, api := range data.API {
+		for _, method := range data.Method {
+			_, err := casbin.Rbac.AddPolicy(data.Role, api, method)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{
+					"status":  "fail",
+					"message": "can not add role for this user",
+				})
+			}
+		}
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"status": "success",
+	})
+}
